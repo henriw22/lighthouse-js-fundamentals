1153330e7118a957cd2ae9bc132ab55ed9be3b9a
Henri William <henriw22@gmail.com>
2021-05-10 05:35:26 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -5,7 +5,11 @@ if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
 
+//test
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
