6a0f6a62c3f02048f54f8703aa5b3880f26a5f4f
Henri William <henriw22@gmail.com>
2021-05-12 22:25:14 +0000

    how to combine two arrays into one

---
+++ concat-arrays.js
@@ -0,0 +1,11 @@
+function concat(tom,jerry) {
+  for (let i = 0; i < jerry.length; i++) {
+    tom.push(jerry[i]);
+  }
+  return tom;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
